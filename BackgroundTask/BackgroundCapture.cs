64e0daab0bf3d1b8c109877ed6bb2151dad52586
Oleksandr Yushchenko <arxalex@arxalex.com>
2020-05-13 10:23:25 +0300

    gg

---
+++ BackgroundTask/BackgroundCapture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace BackgroundTask
+{
+    public sealed class BackgroundCapture : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _deferral;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _deferral = taskInstance.GetDeferral();
+            _deferral.Complete();
+        }
+        
+    }
+}
